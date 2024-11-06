16697a576d64822429dcba682412b1e2563ad367
Ben Kwa <3789589+benkwa@users.noreply.github.com>
2024-11-05 23:58:02 -0800

    Emacs navigation; cleanup plugins

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -2,11 +2,6 @@
 // Copyright 2016 Keyboardio, inc. <jesse@keyboard.io>
 // See "LICENSE" for license details
 
-#ifndef BUILD_INFORMATION
-#define BUILD_INFORMATION "locally built"
-#endif
-
-
 /**
  * These #include directives pull in the Kaleidoscope firmware core,
  * as well as the Kaleidoscope plugins we use in the Model 01's firmware
@@ -15,13 +10,6 @@
 
 // The Kaleidoscope core
 #include "Kaleidoscope.h"
-
-// Support for storing the keymap in EEPROM
-#include "Kaleidoscope-EEPROM-Settings.h"
-#include "Kaleidoscope-EEPROM-Keymap.h"
-
-// Support for communicating with the host via a simple Serial protocol
-#include "Kaleidoscope-FocusSerial.h"
 
 // Support for keys that move the mouse
 #include "Kaleidoscope-MouseKeys.h"
@@ -42,17 +30,8 @@
 // Support for an LED mode that makes all the LEDs 'breathe'
 #include "Kaleidoscope-LEDEffect-Breathe.h"
 
-// Support for an LED mode that makes a red pixel chase a blue pixel across the keyboard
-#include "Kaleidoscope-LEDEffect-Chase.h"
-
 // Support for LED modes that pulse the keyboard's LED in a rainbow pattern
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
-
-// Support for an LED mode that lights up the keys as you press them
-#include "Kaleidoscope-LED-Stalker.h"
-
-// Support for an LED mode that prints the keys you press in letters 4px high
-#include "Kaleidoscope-LED-AlphaSquare.h"
 
 // Support for shared palettes for other plugins, like Colormap below
 #include "Kaleidoscope-LED-Palette-Theme.h"
@@ -87,11 +66,12 @@
   * That switch statement actually runs the code associated with a macro when
   * a macro key is pressed.  */
 
-enum { MACRO_VERSION_INFO,
-       MACRO_ARROW,
-       MACRO_MOUSE_SCREEN_L,
-       MACRO_MOUSE_SCREEN_R,
-     };
+enum {
+    MACRO_VERSION_INFO,
+    MACRO_ARROW,
+    MACRO_MOUSE_SCREEN_L,
+    MACRO_MOUSE_SCREEN_R,
+};
 
 
 
@@ -145,6 +125,8 @@ enum { MACRO_VERSION_INFO,
 
 enum {
     PRIMARY,
+    ALT,
+    CTRL,
     SYMBOL,
     MOUSE,
 }; // layers
@@ -155,6 +137,10 @@ enum {
 #define Key_RightCurly   Key_RightCurlyBracket
 #define Mouse_ScrnL      M(MACRO_MOUSE_SCREEN_L)
 #define Mouse_ScrnR      M(MACRO_MOUSE_SCREEN_R)
+
+#define BackWord LCTRL(Key_LeftArrow)
+#define ForWord LCTRL(Key_RightArrow)
+#define DelWord LCTRL(Key_Delete)
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -170,7 +156,7 @@ KEYMAPS(
           Key_Tab,       Key_A, Key_S, Key_D, Key_F, Key_G,
           Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Meh,
 
-          CTL_T(Backspace), ALT_T(Delete), GUI_T(Home), Key_End,
+          LT(CTRL, Backspace), ALT_T(Delete), CTL_T(Home), Key_End,
           ShiftToLayer(SYMBOL),
 
 
@@ -180,10 +166,53 @@ KEYMAPS(
                                Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
           ShiftToLayer(MOUSE), Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightShift,
 
-          Key_PageDown, GUI_T(PageUp), ALT_T(Enter), CTL_T(Spacebar),
+          Key_PageDown, CTL_T(PageUp), LT(ALT, Enter), CTL_T(Spacebar),
           ShiftToLayer(SYMBOL)
     ),
 
+
+  [ALT] = KEYMAP_STACKED(
+          // Left Hand
+          LALT(Key_Equals),    LALT(Key_1), LALT(Key_2), LALT(Key_3), LALT(Key_4), LALT(Key_5), LALT(Key_Escape),
+          LALT(Key_CapsLock),  LALT(Key_Q), LALT(Key_W), LALT(Key_E), LALT(Key_R), LALT(Key_T), ___,
+          LALT(Key_Tab),       LALT(Key_A), LALT(Key_S), DelWord,     ForWord,     LALT(Key_G),
+          LALT(Key_LeftShift), LALT(Key_Z), LALT(Key_X), LALT(Key_C), LALT(Key_V), BackWord,    ___,
+
+          CTL_T(Backspace), ALT_T(Delete), GUI_T(Home), LALT(Key_End),
+          ShiftToLayer(SYMBOL),
+
+
+          // Right Hand
+          ___, LALT(Key_6), LALT(Key_7), LALT(Key_8),     LALT(Key_9),      LALT(Key_0),         LALT(Key_Minus),
+          ___, LALT(Key_Y), LALT(Key_U), LALT(Key_I),     LALT(Key_O),      LALT(Key_P),         LALT(Key_Backslash),
+               LALT(Key_H), LALT(Key_J), LALT(Key_K),     LALT(Key_L),      LALT(Key_Semicolon), LALT(Key_Quote),
+          ___, LALT(Key_N), LALT(Key_M), LALT(Key_Comma), LALT(Key_Period), LALT(Key_Slash),     LALT(Key_RightShift),
+
+          LALT(Key_PageDown), GUI_T(PageUp), ALT_T(Enter), CTL_T(Spacebar),
+          ShiftToLayer(SYMBOL)
+    ),
+
+
+  [CTRL] = KEYMAP_STACKED(
+          // Left Hand
+          LCTRL(Key_Equals),    LCTRL(Key_1), LCTRL(Key_2), LCTRL(Key_3), LCTRL(Key_4), LCTRL(Key_5), LCTRL(Key_Escape),
+          LCTRL(Key_CapsLock),  LCTRL(Key_Q), LCTRL(Key_X), Key_End,      LCTRL(Key_R), LCTRL(Key_T), ___,
+          LCTRL(Key_Tab),       Key_Home,     LCTRL(Key_S), LCTRL(Key_D), LCTRL(Key_F), LCTRL(Key_G),
+          LCTRL(Key_LeftShift), LCTRL(Key_Z), LCTRL(Key_X), LCTRL(Key_C), LCTRL(Key_V), LCTRL(Key_B), ___,
+
+          CTL_T(Backspace), ALT_T(Delete), GUI_T(Home), LCTRL(Key_End),
+          ShiftToLayer(SYMBOL),
+
+
+          // Right Hand
+          ___, LCTRL(Key_6),  LCTRL(Key_7), LCTRL(Key_8),     LCTRL(Key_9),      LCTRL(Key_0),         LCTRL(Key_Minus),
+          ___, LCTRL(Key_V),  LCTRL(Key_U), LCTRL(Key_I),     LCTRL(Key_O),      Key_UpArrow,          LCTRL(Key_Backslash),
+               LCTRL(Key_H),  LCTRL(Key_J), LCTRL(Key_K),     LCTRL(Key_L),      LCTRL(Key_Semicolon), LCTRL(Key_Quote),
+          ___, Key_DownArrow, LCTRL(Key_M), LCTRL(Key_Comma), LCTRL(Key_Period), LCTRL(Key_Slash),     LCTRL(Key_RightShift),
+
+          LCTRL(Key_PageDown), GUI_T(PageUp), ALT_T(Enter), CTL_T(Spacebar),
+          ShiftToLayer(SYMBOL)
+    ),
 
   [SYMBOL] =  KEYMAP_STACKED(
           Key_Backtick,  Key_F1,  Key_F2,  Key_F3,  Key_F4,  Key_F5,  M(MACRO_VERSION_INFO),
@@ -239,6 +268,12 @@ KEYMAPS(
 /* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
 
+#ifndef BUILD_INFORMATION
+#define BUILD_INFORMATION "locally built on " __DATE__ " at " __TIME__
+#endif
+#define XSTR(s) STR(s)
+#define STR(s) #s
+
 /** versionInfoMacro handles the 'firmware version info' macro
  *  When a key bound to the macro is pressed, this macro
  *  prints out the firmware build information as virtual keystrokes
@@ -246,33 +281,14 @@ KEYMAPS(
 
 static void versionInfoMacro(uint8_t keyState) {
     if (keyToggledOn(keyState)) {
-        Macros.type(PSTR("Keyboardio Model 01 - Kaleidoscope "));
-        Macros.type(PSTR(BUILD_INFORMATION));
+        Macros.type(PSTR("Keyboardio Model 01 - Firmware version "));
+        Macros.type(PSTR(XSTR(BUILD_INFORMATION)));
     }
 }
-
 
 static void arrowOperatorMacro(uint8_t keyState) {
     if (keyToggledOn(keyState)) {
         Macros.type(PSTR("->"));
-    }
-}
-
-static void mouseScreenLeft(uint8_t keyState) {
-    if (keyToggledOn(keyState)) {
-        Kaleidoscope.hid().absoluteMouse().moveTo(0, MAX_WARP_HEIGHT/2, 0);
-        Kaleidoscope.hid().mouse().move(-10, 0, 0, 0);
-        Kaleidoscope.hid().absoluteMouse().moveTo(MAX_WARP_WIDTH/2, MAX_WARP_HEIGHT/2, 0);
-        kaleidoscope::plugin::MouseWrapper_::end_warping();
-    }
-}
-
-static void mouseScreenRight(uint8_t keyState) {
-    if (keyToggledOn(keyState)) {
-        Kaleidoscope.hid().absoluteMouse().moveTo(MAX_WARP_WIDTH, MAX_WARP_HEIGHT/2, 0);
-        Kaleidoscope.hid().mouse().move(10, 0, 0, 0);
-        Kaleidoscope.hid().absoluteMouse().moveTo(MAX_WARP_WIDTH/2, MAX_WARP_HEIGHT/2, 0);
-        kaleidoscope::plugin::MouseWrapper_::end_warping();
     }
 }
 
@@ -299,13 +315,6 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
       arrowOperatorMacro(keyState);
       break;
 
-  case MACRO_MOUSE_SCREEN_L:
-      mouseScreenLeft(keyState);
-      break;
-
-  case MACRO_MOUSE_SCREEN_R:
-      mouseScreenRight(keyState);
-      break;
   }
 
   return MACRO_NONE;
@@ -402,23 +411,6 @@ USE_MAGIC_COMBOS(
 // The order can be important. For example, LED effects are
 // added in the order they're listed here.
 KALEIDOSCOPE_INIT_PLUGINS(
-  // The EEPROMSettings & EEPROMKeymap plugins make it possible to have an
-  // editable keymap in EEPROM.
-  EEPROMSettings,
-  EEPROMKeymap,
-
-  // Focus allows bi-directional communication with the host, and is the
-  // interface through which the keymap in EEPROM can be edited.
-  Focus,
-
-  // FocusSettingsCommand adds a few Focus commands, intended to aid in
-  // changing some settings of the keyboard, such as the default layer (via the
-  // `settings.defaultLayer` command)
-  FocusSettingsCommand,
-
-  // FocusEEPROMCommand adds a set of Focus commands, which are very helpful in
-  // both debugging, and in backing up one's EEPROM contents.
-  FocusEEPROMCommand,
 
   // The boot greeting effect pulses the LED button for 10 seconds after the
   // keyboard is first connected
@@ -444,22 +436,11 @@ KALEIDOSCOPE_INIT_PLUGINS(
   // and slowly moves the rainbow across your keyboard
   LEDRainbowWaveEffect,
 
-  // The chase effect follows the adventure of a blue pixel which chases a red pixel across
-  // your keyboard. Spoiler: the blue pixel never catches the red pixel
-  LEDChaseEffect,
-
   // These static effects turn your keyboard's LEDs a variety of colors
   solidRed, solidOrange, solidYellow, solidGreen, solidBlue, solidIndigo, solidViolet,
 
   // The breathe effect slowly pulses all of the LEDs on your keyboard
   LEDBreatheEffect,
-
-  // The AlphaSquare effect prints each character you type, using your
-  // keyboard's LEDs as a display
-  AlphaSquareEffect,
-
-  // The stalker effect lights up the keys you've pressed recently
-  StalkerEffect,
 
   // The LED Palette Theme plugin provides a shared palette for other plugins,
   // like Colormap below
@@ -498,9 +479,6 @@ void setup() {
   // First, call Kaleidoscope's internal setup function
   Kaleidoscope.setup();
 
-  // We configure the AlphaSquare effect to use RED letters
-  AlphaSquare.color = CRGB(255, 0, 0);
-
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect
   LEDRainbowEffect.brightness(150);
@@ -509,22 +487,10 @@ void setup() {
   // Set the action key the test mode should listen for to Left Fn
   HardwareTestMode.setActionKey(R3C6);
 
-  // The LED Stalker mode has a few effects. The one we like is called
-  // 'BlazingTrail'. For details on other options, see
-  // https://github.com/keyboardio/Kaleidoscope/blob/master/docs/plugins/LED-Stalker.md
-  StalkerEffect.variant = STALKER(BlazingTrail);
-
   // We want to make sure that the firmware starts with LED effects off
   // This avoids over-taxing devices that don't have a lot of power to share
   // with USB devices
   LEDOff.activate();
-
-  // To make the keymap editable without flashing new firmware, we store
-  // additional layers in EEPROM. For now, we reserve space for five layers. If
-  // one wants to use these layers, just set the default layer to one in EEPROM,
-  // by using the `settings.defaultLayer` Focus command, or by using the
-  // `keymap.onlyCustom` command to use EEPROM layers only.
-  EEPROMKeymap.setup(5);
 
   // We need to tell the Colormap plugin how many layers we want to have custom
   // maps for. To make things simple, we set it to five layers, which is how
